75038b43666b68dc1f27732a821c8795b9663d9f
ainaimi <ashley.naimi@pitt.edu>
2018-05-10 00:10:52 -0400

    added bootstrap function and some plots

--- NuMom_Prelim.R
+++ NuMom_Prelim.R
@@ -1,5 +1,5 @@
 #Install and load packages
-packages <- c("tidyverse","tabplot","VIM","rpart","rpart.plot")
+packages <- c("tidyverse","tabplot","VIM","rpart","rpart.plot","expss","here","boot")
 for (package in packages) {
   if (!require(package, character.only=T, quietly=T)) {
     install.packages(package,repos='http://lib.stat.cmu.edu/R/CRAN') 
@@ -10,40 +10,31 @@ for (package in packages) {
   library(package, character.only=T)
 }
 
-install.packages("rpart.plot")
-install.packages("expss")
-
-library("expss")
-library("tidyverse")
-library("tabplot")
-library("VIM")
-library("rpart")
-library("rpart.plot")
-
 ## import data here.
- 
-setwd("/Users/abigailcartus/Box/numom2b diet and machine learning")
-getwd()
-a<- read.csv("numom_small HEI total score.csv", header=TRUE, sep=",")
+a<- read.csv("./numom_small HEI total score.csv", header=TRUE, sep=",")
+
+head(a)
 
 # need to create a dataset that has only the stuff we want
 # Removing studyid, sptb37_alt, and the income variable (pctfedpov)
-a <- a[c(-1,-4,-6)]
+a <- subset(a,select=-c(studyid,sptb37_alt,pctfedpov,bmicat))
 
 # general graphical overview of data
 tableplot(a)
 
 # visualization of missing
 aggr(a)
+nrow(a)
 
 #Low proportion missing; using only complete cases (?)
 a <- a[complete.cases(a), ]
+nrow(a)
 
 #Converting integer variables to factors
 a$sptb37 <- factor(a$sptb37, levels=c(0,1), labels=c("Not preterm", "Preterm"))
-a$smokerpre <- factor(a$smokerpre, levels=c(1,2), labels=c("Nonsmoker","Smoker"))
+a$smokerpre <- factor(a$smokerpre, levels=c(1,2), labels=c("Smoker","Nonsmoker"))
 a$agecat3 <- factor(a$agecat3)
-a$bmicat <- factor(a$bmicat)
+#a$bmicat <- factor(a$bmicat)
 a$white <- factor(a$white)
 a$college <- factor(a$college)
 a$married <- factor(a$married)
@@ -53,12 +44,12 @@ a = apply_labels(a,
                  sptb37 = "Preterm birth",
                  smokerpre = "Smoker",
                  agecat3 = "Age group",
-                 bmicat = "BMI group",
+                 #bmicat = "BMI group",
                  white = "Race",
                  college = "Education",
                  married = "Marital status",
                  heix_tot = "HEI score"
-                 )
+)
 
 #Switching columns in the data frame to see if column order matters
 b <- a[c("bmi", "agecat3", "smokerpre", "white", "bmicat", "college","married","sptb37", "heix_tot")]
@@ -67,7 +58,20 @@ b <- a[c("bmi", "agecat3", "smokerpre", "white", "bmicat", "college","married","
 # to use the "Class ~ ." notation, make sure you only have things in the data that you want in teh model
 # the outcome here is called "Class" (case matters)
 # We have only the variables we want in the model, outcome = sptb37
-tree1 <- rpart(sptb37 ~ ., data=a, method="class", control=rpart.control(minsplit=25, minbucket=3, cp=0.001))
+
+names(a)
+ggplot(a) + geom_histogram(aes(x=heix_tot))
+ggplot(a) + geom_histogram(aes(x=log(bmi)))
+ggplot(a) + geom_point(aes(x=bmi,y=heix_tot))
+
+table(a$smokerpre)
+table(a$agecat3)
+table(a$sptb37)
+table(a$white)
+table(a$college)
+table(a$married)
+
+tree1 <- rpart(sptb37 ~ ., data=a, method="class", control=rpart.control(minsplit=5, minbucket=3, cp=0.001))
 
 #Plotting the tree
 plot(tree1, branch=0.1, uniform=TRUE, compress=TRUE)  
@@ -94,15 +98,52 @@ a0 <- a; a0$heix_tot <- 79.1 #median of HEI quintile 5
 a1 <- a; a1$heix_tot <- 38.9 #overall split score
 
 # predict from each
-y0 <- predict(tree1,newdata = a0)
-y1 <- predict(tree1,newdata = a1)
 
-# here's the "impact" of heix_tot on sptb37 expressed on the risk difference scale
-mean(y1[,2]) - mean(y0[,2])
+pFunc <- function(data,indices,outcome){
+  
+  varname<-eval(substitute(outcome), data)
+  tree1 <- rpart(varname~ ., data=data[indices,], method="class", control=rpart.control(minsplit=25, minbucket=3, cp=0.001))
+  
+  medians <- data[indices,] %>% 
+    group_by(quantiles=ntile(heix_tot,5)) %>% 
+    mutate(medians=median(heix_tot)) %>% 
+    arrange(quantiles) %>%
+    slice(1) %>%
+    select(medians)
+  
+  #print(medians)
+  
+  a0 <- data[indices,]
+  a0 <- a0 %>% mutate(heix_tot=as.numeric(medians[1,2]))
+  a1 <- data[indices,]
+  a1 <- a1 %>% mutate(heix_tot=as.numeric(medians[5,2]))
+  
+  y0 <- predict(tree1,newdata = a0)
+  y1 <- predict(tree1,newdata = a1)
+  
+  # here's the "impact" of heix_tot on sptb37 expressed on the risk difference scale
+  RD<-mean(y1[,2]) - mean(y0[,2])
+  
+  # On the Risk Ratio scale
+  RR<-mean(y1[,2]) / mean(y0[,2])
+  
+  # On the Odds Ratio scale
+  OR<-(mean(y1[,2])/(1 - mean(y1[,2]))) / (mean(y0[,2])/(1 - mean(y0[,2])))
+  
+  results<-cbind(RD,RR,OR)
+  return(results)
+  
+}
 
-# On the Risk Ratio scale
-mean(y1[,2]) / mean(y0[,2])
+## for some reason this yields null effect, and not what was in table
+## bc the tree splits HEI only below the median of Q1??
+pFunc(a,1:nrow(a),sptb37)
 
-# On the Odds Ratio scale
-(mean(y1[,2])/(1 - mean(y1[,2]))) / (mean(y0[,2])/(1 - mean(y0[,2])))
+# bootstrapping
+boot_res <- boot(data=a,statistic=pFunc,R=1000,outcome=sptb37)
+
+## SE of RD, RR, OR
+boot_res$t[,2] <- log(boot_res$t[,2])
+boot_res$t[,3] <- log(boot_res$t[,3])
+apply(boot_res$t,2,sd)
 
